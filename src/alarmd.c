5a7fa0f6e0cd0c676cd13347ea5397d9003f27af
bitabyte <bitabyte7@gmail.com>
2026-04-14 18:05:31 +0530

    check for input time

--- src/alarmd.c
+++ src/alarmd.c
@@ -19,6 +19,25 @@ int main(int argc, char *argv[])
     snprintf(target_time, sizeof(target_time), "%s %s %s %s %s %s",
             argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
     
+    struct tm alarm_tm = {0};
+    alarm_tm.tm_year = atoi(argv[1]) - 1900;
+    alarm_tm.tm_mon = atoi(argv[2]) - 1;
+    alarm_tm.tm_mday = atoi(argv[3]);
+    alarm_tm.tm_hour = atoi(argv[4]);
+    alarm_tm.tm_min = atoi(argv[5]);
+    alarm_tm.tm_sec = atoi(argv[6]);
+    
+    time_t alarm_time = mktime(&alarm_tm);
+    
+    time_t now = time(NULL);
+
+    
+    if(alarm_time < now) { 
+        
+        fprintf(stderr, "ERROR: DON'T GIVE INVALID INPUT YOU FILTHY LITTLE COCKROACH !!!\n");
+        exit(EXIT_FAILURE);
+    }
+
     openlog("alarmd",LOG_PID, LOG_DAEMON);
     
     syslog(LOG_INFO, "Alarm daemon started for %s", target_time);
@@ -28,7 +47,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     } 
     
-    char *sound = "/usr/bin/mpv --no-video --quiet /home/bitabyte/Desktop/alarm.wav &"; // insert your own music file and music player   
+    char *sound = "/usr/bin/mpv --no-video --quiet /home/bitabyte/Desktop/alarm.wav &";
     char buffer[80];
    
     while(1){
@@ -37,7 +56,7 @@ int main(int argc, char *argv[])
         struct tm *info = localtime(&rawtime);
 
         strftime(buffer, sizeof(buffer), "%Y %m %d %H %M %S",info);
-        
+ 
         if(strcmp(target_time,buffer) == 0){
             syslog(LOG_WARNING, "AlARM TRIGGERED!!");
             system(sound);
